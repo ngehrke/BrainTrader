83505993868ec8c2202072d8ecf2a5d8263fec62
ngehrke <nick.gehrke@zapliance.com>
2024-11-09 18:22:17 +0100

    init

---
+++ src/test/java/com/braintrader/tests/dev/TestMain.java
@@ -0,0 +1,18 @@
+package com.braintrader.tests.dev;
+
+import com.ib.client.EClientSocket;
+import org.junit.jupiter.api.Test;
+
+class TestMain {
+
+    @Test
+    void testMain() throws InterruptedException {
+
+        System.out.println("Test");
+
+
+        System.out.println("finished");
+
+    }
+
+}
